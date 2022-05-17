77f1ef66ff666b7c24aca78f7ae423319809ad5f
dmncp <damiancyper@gmail.com>
2022-05-17 12:55:00 +0200

    lab4: elbow method almost done

--- lab4/dataset_generator.py
+++ lab4/dataset_generator.py
@@ -7,18 +7,19 @@ import seaborn as sns
 def get_features():
     return embedding
 
-
-sns.set(context="paper", style="white")
+print('preparing mnist')
+# sns.set(context="paper", style="white")
 
 mnist = fetch_openml("mnist_784", version=1)
-
+print('done, now reducer')
 reducer = umap.UMAP(random_state=42)
+print('done, embedding')
 embedding = reducer.fit_transform(mnist.data)
-
-fig, ax = plt.subplots(figsize=(12, 10))
-color = mnist.target.astype(int)
-plt.scatter(embedding[:, 0], embedding[:, 1], c=color, cmap="Spectral", s=0.1)
-plt.setp(ax, xticks=[], yticks=[])
-plt.title("MNIST data embedded into two dimensions by UMAP", fontsize=18)
-
-plt.show()
+print('embedding done, go to main')
+# fig, ax = plt.subplots(figsize=(12, 10))
+# color = mnist.target.astype(int)
+# plt.scatter(embedding[:, 0], embedding[:, 1], c=color, cmap="Spectral", s=0.1)
+# plt.setp(ax, xticks=[], yticks=[])
+# plt.title("MNIST data embedded into two dimensions by UMAP", fontsize=18)
+#
+# plt.show()
--- lab4/main.py
+++ lab4/main.py
@@ -1,25 +1,57 @@
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.metrics import fowlkes_mallows_score, rand_score
 from dataset_generator import get_features
 import numpy as np
 
 
-def elbow_method(Kmin=1, Kmax=3, no_iterations=10):
+def print_and_save_chart(x, K, x_title, y_title, title, filename):
+    plt.figure(figsize=(16, 8))
+    plt.plot(K, x, 'bx-')
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+    plt.title(title)
+    plt.savefig(f'./outputs/{filename}')
+
+
+def elbow_method(Kmin=1, Kmax=30, no_iterations=100):
     K = range(Kmin, Kmax)
     distortions_avg = []
     distortions_std = []
+    predict_avg = []
+    predict_std = []
 
     for k in K:
         print(f"ELBOW FOR K={k}:")
         results = []
+        predict_results = []
         for i in range(no_iterations):
             print(f"iteration nr.{i}")
             kmeans = KMeans(n_clusters=k)
             kmeans.fit(get_features())
             results.append(kmeans.inertia_)
+
+            # compare with predict result
+            predict_results.append(kmeans.predict(get_features()))
+
         print(results)
         print(np.std(results))
         print(np.average(results))
+        print(predict_results)
+        print(np.std(predict_results))
+        print(np.average(predict_results))
+        distortions_avg.append(np.average(results))
+        distortions_std.append(np.std(results))
+        predict_avg.append(np.average(predict_results))
+        predict_std.append(np.std(predict_results))
+
+    print_and_save_chart(distortions_avg, K, "k", "Distortion AVG", "Elbow method AVG", "elbow_avg")
+    print_and_save_chart(distortions_std, K, "k", "Distortion STD", "Elbow method STD", "elbow_std")
+    print_and_save_chart(predict_avg, K, "k", "Distortion AVG (predict)", "Elbow method AVG predict", "elbow_avg_predict")
+    print_and_save_chart(predict_std, K, "k", "Distortion STD (predict)", "Elbow method STD predict", "elbow_std_predict")
+
+    print(f"Fowlkes-Mallows score: {fowlkes_mallows_score(distortions_avg, predict_avg)}")
+    print(f"Rand score: {rand_score(distortions_avg, predict_avg)}")
 
 
 if __name__ == '__main__':
